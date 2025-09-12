c00ea6facb2874929bd0eea657a2077f842b6c3c
Geoff Zhu <zhuge2@oocl.com>
2025-09-12 15:28:44 +0800

    feat: add done to do list

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,19 +1,26 @@
 import {useContext} from "react";
 import {TodoContext} from "../contexts/TodoContext";
 import './TodoList.css';
-const TodoList = () => {
-  const {state, dispatch} = useContext(TodoContext)
-  return (
-      <div className={'todo-group'}>
-        <div>This is the TodoList Component.</div>
-        {
-          state.map(todo => {
-            return <div className={'todo-item'}>{todo.text}</div>
-          })
-        }
-      </div>
 
-  );
+const TodoList = () => {
+    const {state, dispatch} = useContext(TodoContext)
+
+    function toggleDone(id) {
+        dispatch({type: 'DONE', id: id})
+    }
+
+    return (
+        <div className={'todo-group'}>
+            <div>This is the TodoList Component.</div>
+            {
+                state.map(({id, text, done}) => {
+                    return <div className={`todo-item ${done? 'done':''}`}
+                                onClick={() => toggleDone(id)}>{text}</div>
+                })
+            }
+        </div>
+
+    );
 }
 
 export default TodoList
