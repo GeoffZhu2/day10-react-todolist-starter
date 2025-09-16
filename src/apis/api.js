b97b4fb38ef91b641c0cc739e7ff44308e9f42b5
Geoff Zhu <zhuge2@oocl.com>
2025-09-16 18:27:19 +0800

    feat:change the api to backend

--- src/apis/api.js
+++ src/apis/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-    baseURL: "https://68c78d205d8d9f51473226c2.mockapi.io/api"
+    baseURL: "http://localhost:8080"
 });
 
 export const getTodos = async () => {
--- src/reducers/todoReducer.js
+++ src/reducers/todoReducer.js
@@ -1,7 +1,4 @@
-export const initialState = [
-    {id: 1, text: "the first todo", done: false},
-    {id: 2, text: "the second todo", done: false},
-];
+export const initialState = [];
 
 // reducer is a pure function that define and gather all state update logic
 export const todoReducer = (state, action) => {
